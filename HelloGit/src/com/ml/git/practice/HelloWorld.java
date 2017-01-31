b74f2d8ead98c8c3775fc02c76a55088d8cb322a
RamaPrasad <ram250391@gmail.com>
2017-01-30 20:40:21 -0500

    First Commit

---
+++ HelloGit/src/com/ml/git/practice/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.ml.git.practice;
+
+/**
+ * @author ram25
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Helllo World First Time");
+
+	}
+
+}
